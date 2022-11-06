8b27cd61eec4d7935425e551c2ffee9ec5b7b0e4
agupt0318 <anantg001@gmail.com>
2022-11-06 17:56:27 -0500

    Added grade to Student object

--- Student.java
+++ Student.java
@@ -1,16 +1,18 @@
 public class Student {
     String name;
+    int grade;
 
-    public Student(String n) {
+    public Student(String n, int g) {
         name = n;
+        grade = g;
     }
 
     public void printStudent() {
-        System.out.println(name);
+        System.out.println(name + " " + grade);
     }
 
     public static void main(String[] args) {
-        Student b = new Student("Jhon Doe");
+        Student b = new Student("Jhon Doe", 9);
         b.printStudent();
     }
 }
